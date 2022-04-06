d6e2d83bc33cb9563535897f2444f2465ecd153d
Marcelo Tosco @capynet <capy.net@gmail.com>
2022-04-06 12:22:11 +0200

    Now once the user selected the side, the screen turns into a more aligned color to it feelings.

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
 import {RouterLink, RouterView} from 'vue-router'
+import {useGeneralPreferencesStore} from '@/stores/generalPreferences'
+
+const store = useGeneralPreferencesStore();
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/people">People</RouterLink>
-        <!--        <SearchPrefetchedPersons />-->
-        <!--        <SearchPersonsFromAPI />-->
-      </nav>
-    </div>
-  </header>
 
-  <RouterView/>
+  <main :class="{'light-side' : store.side === 'light', 'dark-side' : store.side === 'dark', }">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/people">People</RouterLink>
+          <!--        <SearchPrefetchedPersons />-->
+          <!--        <SearchPersonsFromAPI />-->
+        </nav>
+      </div>
+    </header>
 
+    <RouterView/>
+  </main>
 </template>
+
+<style scoped>
+main.light-side {
+  background: #fff;
+  color: #000;
+}
+
+main.light-side a {
+  color: #000;
+}
+
+main.dark-side {
+  background: #000;
+  color: #fff;
+}
+
+main.dark-side a {
+  color: #fff;
+}
+</style>
