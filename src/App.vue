1d6094b24dafab7eca6afc9028250aa9ac3e346d
Marcelo Tosco @capynet <capy.net@gmail.com>
2022-04-08 12:31:32 +0200

    Styled SplashSelectSide.vue component.

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,17 @@ const store = useGeneralPreferencesStore();
 </script>
 
 <template>
-    <SplashSelectSide v-if="!store.side"/>
-    <MainScreen v-if="store.side"/>
+  <SplashSelectSide v-if="!store.side"/>
+  <MainScreen v-if="store.side"/>
 </template>
 
 <style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
 </style>
